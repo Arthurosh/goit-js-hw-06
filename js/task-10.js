714f4e0e54928b33771bd2aac89ef856bdef23e1
ArthurS <arthurosh@gmail.com>
2022-08-31 11:27:09 +0200

    Adds a question

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,21 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const controlsEl = document.querySelector("#controls");
+const boxesEl = document.querySelector("#boxes");
+
+const inputEl = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+// Прошу дати підказку в якому напрямку рухатись далі, тому що зовсім щось
+// не можу зрозуміти як вирішити дане питання:
+// Кожен елемент після першого повинен бути ширшим і вищим
+// від попереднього на 10px.
