d5f2163a4dc4b637fb67f321f2a05d806c1f2fbf
ArthurS <arthurosh@gmail.com>
2022-08-31 11:25:55 +0200

    adds the execution of task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    if (value === "") {
+      return alert("Всі поля повинні бути заповнені");
+    } else {
+      console.log({ name, value });
+      form.reset();
+    }
+  });
+}
