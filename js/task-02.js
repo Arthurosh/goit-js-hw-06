9b00731150d788668d12961719f717df24bed223
ArthurS <arthurosh@gmail.com>
2022-08-31 11:24:26 +0200

    adds the execution of task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createEl = ingredients.map((ingredient) => {
+  const liItemEl = document.createElement("li");
+  liItemEl.classList = "item";
+  liItemEl.textContent = ingredient;
+  return liItemEl;
+});
+
+const ulEl = document.querySelector("#ingredients");
+ulEl.append(...createEl);
+
+console.log(ulEl);
