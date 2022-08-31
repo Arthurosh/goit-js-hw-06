4418e4fdd5f613aba2e7ed9861b8190e345611fb
ArthurS <arthurosh@gmail.com>
2022-08-31 11:26:15 +0200

    adds the execution of task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+  bodyEl: document.body,
+  spanColorEl: document.querySelector(".color"),
+  btnColorEl: document.querySelector(".change-color"),
+};
+
+refs.btnColorEl.addEventListener("click", changeBg);
+
+function changeBg() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.spanColorEl.textContent = getRandomHexColor();
 }
